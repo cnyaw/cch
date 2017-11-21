5506169a772d69677d0043707a2edad90496f90c
cnyaw <wayncc@gmail.com>
2017-11-22 07:21:05 +0800

    Refine.

--- cch.js
+++ cch.js
@@ -274,18 +274,13 @@ function getMoveRecords(s, p) {
         if (null == v) {
           continue;
         }
-        var found = false;
         for (var i = 0; i < v.length; i++) {
           if (-1 != v[i].indexOf(no)) {
             var rec = s.substring(s.indexOf(')') + 1, s.indexOf(v[i])).trim().split(' ');
             movRec = rec.slice(0, rec.length - 1).concat(movRec);
             mov1 = mov1 + rec.length - 1;
-            found = true;
-            break;
+            return;                     // Found.
           }
-        }
-        if (found) {
-          break;
         }
       }
     }
